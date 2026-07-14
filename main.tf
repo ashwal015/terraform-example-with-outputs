43c21371eb4ca406a2e4e3a44d2affac16bac159
ashwal015 <ashwalse015@gmail.com>
2026-07-14 18:03:15 +0200

    fixed ssh and git ignore

--- main.tf
+++ main.tf
@@ -1,6 +1,31 @@
 provider "aws" {
   region = "eu-central-1"
 }
+resource "aws_security_group" "web_sg" {
+  name        = "web_sg"
+  description = "Allow SSH and HTTP"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
 
 resource "aws_instance" "first_instance" {
   count         = var.instance_count
